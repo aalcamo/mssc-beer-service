65dc1128aab6a23ed5ece0955c5098b2c67041ac
aalcamo <aalcamo@gofurthercorp.com>
2020-08-25 08:54:12 -0400

    created beer controller. closes #3

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v2/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+
+        // TODO: implement
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+
+        // TODO: implement
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+
+        // TODO: implement
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+}
