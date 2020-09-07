a87f4943a5f85fe5b2fcd034f2fd0948d35c9749
aalcamo <aalcamo@gofurthercorp.com>
2020-09-07 17:30:00 -0400

    added Docker containerization.

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-05T16:08:35-0400",
+    date = "2020-09-07T11:27:41-0400",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
 )
 @Component
